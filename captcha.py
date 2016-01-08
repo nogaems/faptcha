1dba1e601270b4f385facc87709db477e1ace119
nogaems <nomad@ag.ru>
2016-01-08 18:36:01 +0300

    added an example of CAPTCHA

--- captcha.py
+++ captcha.py
@@ -94,8 +94,8 @@ class Captcha:
         out = self._noise(out)
 
         # for debug
-        # out.show()
-        # print(key)
+        out.show()
+        print(key)
 
         f = BytesIO()
         out.save(f, 'PNG')
@@ -142,3 +142,6 @@ class Captcha:
             return True
         else:
             return False
+
+c = Captcha()
+c.get()
